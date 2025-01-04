4f5544f2637423924a8dbdf665c163cad46eb668
Bryan Wu <bryan@diffuse.bio>
2025-01-04 03:44:42 +0000

    looks like the access pattern does help with at least the smaller matrices

--- lab2/transpose_device.cu
+++ lab2/transpose_device.cu
@@ -78,20 +78,29 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
     // Data needs enough to handle a single block.
     // Adding enough for padding--33/32 for each
     // __shared__ float data[64 * 64];
-    __shared__ float data[65 * 65];
+    __shared__ float data[64 * 64];
 
     const int i = threadIdx.x + 64 * blockIdx.x;
     int j = 4 * threadIdx.y + 64 * blockIdx.y;
     const int end_j = j + 4;
 
-    // No bank conflict: stride by 33 instead
+    // // No bank conflict: stride by 33 instead
+    // Block reads from data
     for (; j < end_j; j++) {
-        data[(j + 65 * i)] = input[i + n * j];
+        data[((j % 64) + 64 * (i % 64))] = input[i + n * j];
         // data[(j + 64 * i) + i] = input[i + n * j];
     }
     __syncthreads();
-    for (; j < end_j; j++) {
-        output[i + n * j] = data[i + 65 * j];
+    // i_out needs to iterate over whatever j was.
+    // i_out needs to iterate over whatever i was.
+    int i_out = threadIdx.x + 64 * blockIdx.y;
+    int j_out = 4 * threadIdx.y + 64 * blockIdx.x;
+    int end_j_out = j_out + 4;
+    for (; j_out < end_j_out; j_out++) {
+        output[j_out * n + i_out] = data[(i_out % 64) + 64 * (j_out % 64)];
+
+        // output[j + n * i] = 999;
+
     }
 }
 
@@ -100,6 +109,12 @@ void optimalTransposeKernel(const float *input, float *output, int n) {
     // TODO: This should be based off of your shmemTransposeKernel.
     // Use any optimization tricks discussed so far to improve performance.
     // Consider ILP and loop unrolling.
+
+    // Results:
+    // shmem: 0.014336 shmem for 512, 0.574464 for 4096
+    // No bank conflict: 0.009216 for 512, 0.577536 for 4096
+    // Unrolled: 0.009216 for 512, 0.585728 (??) for 4096
+    // ILP
 
     // Data needs enough to handle a single block.
     // Adding enough for padding--33/32 for each
@@ -110,15 +125,51 @@ void optimalTransposeKernel(const float *input, float *output, int n) {
     int j = 4 * threadIdx.y + 64 * blockIdx.y;
     const int end_j = j + 4;
 
-    // No bank conflict: stride by 33 instead
+    // // No bank conflict: stride by 33 instead
+    // Block reads from data
+    // With this, we have 0.011264 for size 512, and 0.574464 for size 4096.
     for (; j < end_j; j++) {
-        data[(j + 65 * i)] = input[i + n * j];
+        data[((j % 64) + 65 * (i % 64))] = input[i + n * j];
         // data[(j + 64 * i) + i] = input[i + n * j];
     }
+
+    // And now unroll loop
+    // data[((j % 64) + 65 * (i % 64))] = input[i + n * j];
+    // data[(((j+1) % 64) + 65 * (i % 64))] = input[i + n * (j+1)];
+    // data[(((j+2) % 64) + 65 * (i % 64))] = input[i + n * (j+2)];
+    // data[(((j+3) % 64) + 65 * (i % 64))] = input[i + n * (j+3)];
+
+    // // And ILP: Haven't finished this yet.
+    // int data_start_ind = (j % 64) + 65 * (i % 64);
+    // int data_start_ind_2 = data_start_ind + 1;
+    // int data_start_ind_3 = data_start_ind + 2;
+    // int data_start_ind_4 = data_start_ind + 3;
+
+    // int input_start_ind = i + n * j;
+    // int input_start_ind_2 = input_start_ind + n;
+    // int input_start_ind_3 = input_start_ind + 2 * n;
+    // int input_start_ind_4 = input_start_ind + 3 * n;
+    // data[data_start_ind] = input[input_start_ind];
+    // data[data_start_ind_2] = input[input_start_ind_2];
+    // data[data_start_ind_3] = input[input_start_ind_3];
+    // data[data_start_ind_4] = input[input_start_ind_4];
     __syncthreads();
-    for (; j < end_j; j++) {
-        output[i + n * j] = data[i + 65 * j];
+    // i_out needs to iterate over whatever j was.
+    // i_out needs to iterate over whatever i was.
+    int i_out = threadIdx.x + 64 * blockIdx.y;
+    int j_out = 4 * threadIdx.y + 64 * blockIdx.x;
+    int end_j_out = j_out + 4;
+
+    for (; j_out < end_j_out; j_out++) {
+        output[j_out * n + i_out] = data[(i_out % 64) + 65 * (j_out % 64)];
+        // output[j + n * i] = 999;
     }
+    // And now unroll loop
+    // output[j_out * n + i_out] = data[(i_out % 64) + 65 * (j_out % 64)];
+    // output[((j_out+1) * n + i_out)] = data[((i_out % 64) + 65 * ((j_out+1) % 64))];
+    // output[((j_out+2) * n + i_out)] = data[((i_out % 64) + 65 * ((j_out+2) % 64))];
+    // output[((j_out+3) * n + i_out)] = data[((i_out % 64) + 65 * ((j_out+3) % 64))];
+
 }
 
 void cudaTranspose(
