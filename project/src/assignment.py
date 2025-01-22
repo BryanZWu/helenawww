b85dab41789fe2ecfd2e3d3a704939e09e377924
Helena Su <helenasu@berkeley.edu>
2025-01-22 15:31:56 -0800

    attempted hw

--- project/src/assignment.py
+++ project/src/assignment.py
@@ -15,7 +15,7 @@ def vanilla_attention_forward(q, k, v):
     D = q.shape[-1] 
     # k dimension: (2, 256, 128), we want 2, 128, 256
     transposed_k = jnp.transpose(k, (0, 2, 1)) 
-    softmax = jax.nn.softmax(q @ transposed_k  / math.sqrt(D), axis=-1) # for each query, the similar scores for all keys should sum to 1
+    softmax = jax.nn.softmax(q @ transposed_k  / math.sqrt(D), axis=2) # for each query, the similar scores for all keys should sum to 1
     out = jnp.matmul(softmax, v)# apply to v
     return out
 
@@ -28,7 +28,21 @@ def sdpa_with_mha_and_mask(q, k, v, mask, num_heads):
     # it in the sequence).
     # Multi-head attention is simply having different sets of QKV that can 
     # each compute its own attention matrix and handle a different concept.
-    ...
+    D = q.shape[-1] # split d by num of heads
+    # k dimension: (2, 256, 128), we want 2, 128, 256
+    # convert BxNxD to Bxnum_headsxNxquery_dim, where query_dim = D/num_heads
+    query_dim = D//num_heads
+    
+    q = q.reshape(B, N, num_heads, query_dim).transpose(0, 2, 1, 3)
+    k = k.reshape(B, N, num_heads, query_dim).transpose(0, 2, 1, 3) #  Bxnum_headsxNxquery_dim
+    attn = q @ k.transpose(0, 1, 3, 2)  #  K.t should be Bxnum_headsxquery_dimxN
+    mask_expanded = jnp.expand_dims(mask, axis=1)
+    masked = attn + mask_expanded # apply mask, cant be multipication since 0 matters in softmax
+    softmax = jax.nn.softmax(masked / math.sqrt(D), axis=-1) # for each query, the similar scores for all keys should sum to 1
+    v_reshaped = v.reshape(B, N, num_heads, query_dim) 
+    v_transpose = v_reshaped.transpose(0, 2, 1, 3)
+    out = jnp.matmul(softmax, v_transpose)# apply to v
+    return out
 
 def triangle_attn(q, k, v):
     # Q3
@@ -43,21 +57,27 @@ def triangle_attn(q, k, v):
     # It logically translates to "For each item B in the batch, for each
     # from(N_f)-to(N_t) pair, here is a N_t attention score corresponding "
     # to each of the N_t nodes that also start at N_f.
-    ...
-
+    D = q.shape[-1] 
+    expanded_q = jnp.expand_dims(q, axis=2)  # Shape becomes (b, n, 1, d)
+    expanded_q = jnp.broadcast_to(expanded_q, (B, N, N, D))  # Shape becomes (b, n, n, d)
+    expanded_k = jnp.expand_dims(k, axis=2)  
+    expanded_k = jnp.broadcast_to(expanded_k, (B, N, N, D)) 
+    # k dimension: (2, 256, 256, 128), we want 2, 256, 128, 256
+    transposed_k = jnp.transpose(expanded_k, (0, 1, 3, 2)) 
     # Apply softmax to the attention scores
-    ...
+    softmax = jax.nn.softmax(expanded_q @ transposed_k  / math.sqrt(D), axis=2) # for each query, the similar scores for all keys should sum to 1
+    
 
     # Apply the attention scores to the values
     # This logically translates to "For each item B in the batch, for each
     # from(N_f)-to(N_t) pair, apply the N_t attention scores to the N_t values
     # that also start at N_f.
-    ...
-
+    expanded_v = jnp.expand_dims(q, axis=2)  # Shape becomes (b, n, 1, d)
+    expanded_v = jnp.broadcast_to(expanded_q, (B, N, N, D))  # Shape becomes (b, n, n, d)
+    out = jnp.matmul(softmax, expanded_v)# apply to v
     # Return the (B, N, N, D) output
-    ...
+    return out
 
-    return ...
 def main(): 
     # First, make sure that the vanilla attention forward pass works
     # is it this command pip install -U "jax[cuda12]" torch? ohkay
@@ -76,7 +96,7 @@ def main():
 
     # Q2
     mask = jax.random.uniform(key, (B, N, N)) > 0.5
-    out2 = ...(q, k, v, mask=mask, num_heads=8)
+    out2 = sdpa_with_mha_and_mask(q, k, v, mask=mask, num_heads=8)
     expected2 = sdpa_with_mha_and_mask_solution(q, k, v, mask=mask, num_heads=8)
     assert jnp.allclose(out2, expected2)
     print("Q2 is good!")
