7c1fb0136b5286d4e997cea85444a50d3bb8ec10
Bryan <bryanwu@berkeley.edu>
2025-01-22 08:23:12 +0000

    oops

--- project/src/assignment.py
+++ project/src/assignment.py
@@ -3,7 +3,7 @@ from jax import numpy as jnp
 import jax
 import torch
 import math
-from reference import scaled_dot_product, sdpa_with_mha_and_mask, triangle_attn
+from reference import scaled_dot_product_solution, sdpa_with_mha_and_mask_solution, triangle_attn_solution
 
 B, N, D = 2, 256, 128
 
@@ -12,7 +12,7 @@ def vanilla_attention_forward(q, k, v):
     # Implement the vanilla attention forward pass: 
     # attn_out = Q @ K^T / sqrt(D)
     ...
-    D = q.shape[-1] # its fine just push the code to edit locally
+    D = q.shape[-1] 
     return q @ k^T / sqrt(D)
 
 def main(): 
@@ -25,21 +25,21 @@ def main():
     q = jax.random.uniform(key, (B, N, D))
     k = jax.random.uniform(key, (B, N, D))
     v = jax.random.uniform(key, (B, N, D))
-    out1 = scaled_dot_product(q, k, v)
-    expected1 = vanilla_attention_forward(q, k, v)
+    expected1 = scaled_dot_product_solution(q, k, v)
+    out1 = vanilla_attention_forward(q, k, v)
     assert jnp.allclose(out1, expected1)
     print("Q1 is good!")
 
     # Q2
     mask = jax.random.uniform(key, (B, N, N)) > 0.5
-    out2 = sdpa_with_mha_and_mask(q, k, v, mask=mask, num_heads=8)
-    expected2 = vanilla_attention_forward(q, k, v)
+    out2 = ...(q, k, v, mask=mask, num_heads=8)
+    expected2 = sdpa_with_mha_and_mask_solution(q, k, v, mask=mask, num_heads=8)
     assert jnp.allclose(out2, expected2)
     print("Q2 is good!")
 
     # Q3
     out3 = triangle_attn(q, k, v)
-    expected3 = vanilla_attention_forward(q, k, v)
+    expected3 = triangle_attn_solution(q, k, v)
     assert jnp.allclose(out3, expected3)
     print("Q3 is good!")
 
