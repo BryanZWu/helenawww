0c5fda10e7642757e38b1d46f950f1beb5af2e2a
Bryan <bryanwu@berkeley.edu>
2025-02-24 07:12:15 +0000

    more naming

--- project/scratch/presentation_code/flash_tt_attn.py
+++ project/scratch/presentation_code/flash_tt_attn.py
@@ -9,7 +9,7 @@ Modifications:
   query-key similarity (as in standard attention [95]),
   but also modulated by the information bjk derived from
   the third edge jk of this triangle"
-- Triangle attention input is pw for all QKV: (B, H, N, N, D)
+- Triangle attention input is pw for all QKV: (B, H, L, L, D)
 
 Notes on nomenclature:
 - Shapes:
@@ -52,12 +52,12 @@ def _attn_fwd_inner(acc, l_i, m_i, q,  #
                     start_ql2, pre_bias_qk_scale, post_bias_qk_scale, #
                     BLOCK_QL2: tl.constexpr, H: tl.constexpr, BLOCK_KL2: tl.constexpr,  #
                     offs_ql2: tl.constexpr, offs_kl2: tl.constexpr,  #
-                    N_CTX: tl.constexpr, fp8_v: tl.constexpr):
+                    L: tl.constexpr, fp8_v: tl.constexpr):
     """
     Inner kernel for attention forward pass computation
     Computes attention scores and updates accumulator for a block of the attention matrix
     """
-    lo, hi = 0, N_CTX
+    lo, hi = 0, L
 
     # Advance block pointers to correct starting positions
     K_block_ptr = tl.advance(K_block_ptr, (0, lo))
@@ -125,7 +125,7 @@ def keep(conf):
     return True
 
 
-@triton.autotune(list(filter(keep, configs)), key=["n", "d"])
+@triton.autotune(list(filter(keep, configs)), key=["L", "D"])
 @triton.jit
 def _attn_fwd(Q, K, V, B_pw, sm_scale, M, Out,  # sm_scale: scaling factor for softmax, M: max values, Out: output tensor
               stride_qb, stride_qh, stride_ql1, stride_ql2, stride_qd,  # Strides for Q tensor: batch, head, seq_len, seq_len, head_dim
@@ -140,19 +140,19 @@ def _attn_fwd(Q, K, V, B_pw, sm_scale, M, Out,  # sm_scale: scaling factor for s
               ):
     """
     Attention forward pass kernel: 
-    Q: [B, H, N1, QL2, D]
-    K: [B, H, N1, KL2, D]
-    V: [B, H, N1, KL2, D]
+    Q: [B, H, L1, QL2, D]
+    K: [B, H, L1, KL2, D]
+    V: [B, H, L1, KL2, D]
     B: [B, H, QL2, KL2]
-    (Note: in practice all N is equal since we work with a pw representation)
+    (Note: in practice all L is equal since we work with a pw representation)
 
 
     Naive implementation:
-    1. attn_logits = Q @ K.transpose(-2, -1) # [B, H, N, N, N]
+    1. attn_logits = Q @ K.transpose(-2, -1) # [B, H, L, L, L]
     2. attn_logits = attn_logits / math.sqrt(head_dim)
-    3. attn_logits = attn_logits + bjk # [B, H, N, N, N]
-    4. attn_score = softmax(attn_logits, axis=-1) # [B, H, N, N, N]
-    5. attn_out = attn_score @ V # [B, H, N, N, D]
+    3. attn_logits = attn_logits + bjk # [B, H, L, L, L]
+    4. attn_score = softmax(attn_logits, axis=-1) # [B, H, L, L, L]
+    5. attn_out = attn_score @ V # [B, H, L, L, D]
 
     Of these, step 3 is not handled by out-of-the-box flash attention.
     """
@@ -160,9 +160,9 @@ def _attn_fwd(Q, K, V, B_pw, sm_scale, M, Out,  # sm_scale: scaling factor for s
     tl.static_assert(BLOCK_KL2 <= D, "Block size (BLOCK_KL2) must not exceed head dimension (D)")
 
     # Grid dimension explanation:
-    # - program_id(0): Indexes along inner sequence length (N_CTX/BLOCK_QL2 blocks)
+    # - program_id(0): Indexes along inner sequence length (L/BLOCK_QL2 blocks)
     #      the N along which we aggregate q and k
-    # - program_id(1): Indexes along outer sequence length (N_CTX blocks)
+    # - program_id(1): Indexes along outer sequence length (L blocks)
     # - program_id(2): Indexes along batch*heads dimension (Z*H blocks)
     start_ql2 = tl.program_id(0) 
     off_l1 = tl.program_id(1)
@@ -172,7 +172,7 @@ def _attn_fwd(Q, K, V, B_pw, sm_scale, M, Out,  # sm_scale: scaling factor for s
     off_b = off_bh // H
     off_h = off_bh % H
 
-    # Calculate base offset for current batch and head, and also first dim of N
+    # Calculate base offset for current batch and head, and also first L dim
     # (which is effectively just another dim to batch along for now)
     # Note that we assume similar strides for Q, K, V, as the original
     # triton flash attention kernel does.
@@ -267,9 +267,9 @@ def _attn_fwd(Q, K, V, B_pw, sm_scale, M, Out,  # sm_scale: scaling factor for s
 
 
 configs_tma = [
-    triton.Config({'BLOCK_QL2': BM, 'BLOCK_KL2': BN}, num_stages=s, num_warps=w) \
-    for BM in [64, 128]\
-    for BN in [32, 64, 128]\
+    triton.Config({'BLOCK_QL2': B_QL2, 'BLOCK_KL2': B_KL2}, num_stages=s, num_warps=w) \
+    for B_QL2 in [64, 128]\
+    for B_KL2 in [32, 64, 128]\
     for s in [2, 3, 4, 6]\
     for w in [4, 8]\
 ]
@@ -278,7 +278,7 @@ configs_tma = [
 
 class _attention(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, q, k, v, b, sm_scale):
+    def forward(ctx, q, k, v, b_pw, sm_scale):
         HEAD_DIM_Q, HEAD_DIM_K = q.shape[-1], k.shape[-1]
         HEAD_DIM_V = v.shape[-1]
         assert HEAD_DIM_Q == HEAD_DIM_K and HEAD_DIM_K == HEAD_DIM_V
@@ -289,7 +289,7 @@ class _attention(torch.autograd.Function):
 
         # Initialize scaling factor storage
         # M is the max val for each query position--QL2.
-        # Shape (B, H, N1, L2), equivalent to attn_scores.max(dim=-1)
+        # Shape (B, H, L1, L2), equivalent to attn_scores.max(dim=-1)
         M = torch.empty((q.shape[0], q.shape[1], q.shape[2], q.shape[3]), device=q.device, dtype=torch.float32)
 
         def grid(args):
@@ -299,14 +299,15 @@ class _attention(torch.autograd.Function):
 
         # Launch regular kernel
         _attn_fwd[grid](
-            q, k, v, b, sm_scale, M, o,
+            q, k, v, b_pw, sm_scale, M, o,
             q.stride(0), q.stride(1), q.stride(2), q.stride(3), q.stride(4),
             k.stride(0), k.stride(1), k.stride(2), k.stride(3), k.stride(4),
             v.stride(0), v.stride(1), v.stride(2), v.stride(3), v.stride(4),
             o.stride(0), o.stride(1), o.stride(2), o.stride(3), o.stride(4),
-            b.stride(0), b.stride(1), b.stride(2), b.stride(3),
-            b=q.shape[0], h=q.shape[1], n=q.shape[2],
-            d=HEAD_DIM_K,)
+            b_pw.stride(0), b_pw.stride(1), b_pw.stride(2), b_pw.stride(3),
+            B=q.shape[0], H=q.shape[1], L=q.shape[2],
+            D=HEAD_DIM_K,
+        )
 
         # Save tensors needed for backward pass
         ctx.save_for_backward(q, k, v, o, M)
@@ -317,8 +318,8 @@ class _attention(torch.autograd.Function):
 attention = _attention.apply
 
 
-@pytest.mark.parametrize("Z, H, N_CTX, HEAD_DIM", [(1, 2, 1024, 32)])
-def test_op(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
+@pytest.mark.parametrize("Z, H, L, HEAD_DIM", [(1, 2, 1024, 32)])
+def test_op(Z, H, L, HEAD_DIM, dtype=torch.float16):
     """
     Test function for attention implementation
     Compares Triton implementation with PyTorch reference implementation
@@ -327,10 +328,10 @@ def test_op(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
     torch.manual_seed(20)
 
     # Initialize input tensors
-    q = (torch.empty((Z, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
-    k = (torch.empty((Z, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
-    v = (torch.empty((Z, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
-    b = (torch.empty((Z, H, N_CTX, N_CTX), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
+    q = (torch.empty((Z, H, L, L, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
+    k = (torch.empty((Z, H, L, L, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
+    v = (torch.empty((Z, H, L, L, HEAD_DIM), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
+    b = (torch.empty((Z, H, L, L), dtype=dtype, device=DEVICE).normal_(mean=0.0, std=0.5).requires_grad_())
     sm_scale = 0.5
     dout = torch.randn_like(q)
 
@@ -364,37 +365,17 @@ def test_op(Z, H, N_CTX, HEAD_DIM, dtype=torch.float16):
 
 TORCH_HAS_FP8 = hasattr(torch, 'float8_e5m2')
 # BATCH, N_HEADS, HEAD_DIM = 4, 32, 64 # To test on A100
-BATCH, N_HEADS, HEAD_DIM = 4, 8, 32 # To test on L4
+B, H, D = 4, 8, 32 # To test on L4
 # vary seq length for fixed head and batch=4
 configs = []
 for mode in ["fwd", "bwd"]:
     if mode == "bwd":
         continue
     
-    # Not supporting flops for now
-    # configs.append(
-    #     triton.testing.Benchmark(
-    #         x_names=["N_CTX"],
-    #         x_vals=[2**i for i in range(6, 13)],
-    #         line_arg="provider",
-    #         line_vals=["triton-fp16", "vanilla-torch"], # "sdpa-kernel"],
-    #         line_names=["Triton [FP16]", "Vanilla PyTorch"], # "SDPA Kernel"],
-    #         # line_vals=["triton-fp16", "vanilla-torch", "sdpa-kernel"],
-    #         # line_names=["Triton [FP16]", "Vanilla PyTorch", "SDPA Kernel"],
-    #         styles=[("red", "-"), ("blue", "-"), ("green", "-")],
-    #         ylabel="TFLOPS",
-    #         plot_name=f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-{mode}-tflops",
-    #         args={
-    #             "H": N_HEADS,
-    #             "BATCH": BATCH,
-    #             "HEAD_DIM": HEAD_DIM,
-    #             "mode": mode,
-    #         },
-    #     ))
-    # Add memory benchmark
+    # Peak memory usage benchmark
     configs.append(
         triton.testing.Benchmark(
-            x_names=["N_CTX"],
+            x_names=["L"],
             x_vals=[2**i for i in range(4, 13)],
             line_arg="provider",
             line_vals=["triton", "vanilla-torch"], # "sdpa-kernel"],
@@ -403,11 +384,11 @@ for mode in ["fwd", "bwd"]:
             # line_names=["Triton [FP16]", "Vanilla PyTorch", "SDPA Kernel"],
             styles=[("red", "-"), ("blue", "-"), ("green", "-")],
             ylabel="Memory (GB)",
-            plot_name=f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-{mode}-memory",
+            plot_name=f"fused-attention-batch{B}-head{H}-d{D}-{mode}-memory",
             args={
-                "H": N_HEADS,
-                "BATCH": BATCH,
-                "HEAD_DIM": HEAD_DIM,
+                "H": H,
+                "B": B,
+                "D": D,
                 "mode": mode,
                 "metric": "memory",
             },
@@ -415,25 +396,25 @@ for mode in ["fwd", "bwd"]:
     # Add latency benchmark
     configs.append(
         triton.testing.Benchmark(
-            x_names=["N_CTX"],
+            x_names=["L"],
             x_vals=[2**i for i in range(4, 13)],
             line_arg="provider",
             line_vals=["triton-fp16", "vanilla-torch"], # "sdpa-kernel"],
             line_names=["Triton [FP16]", "Vanilla PyTorch"], # "SDPA Kernel"],
             styles=[("red", "-"), ("blue", "-"), ("green", "-")],
             ylabel="Time (ms)",
-            plot_name=f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-{mode}-latency",
+            plot_name=f"fused-attention-batch{B}-head{H}-d{D}-{mode}-latency",
             args={
-                "H": N_HEADS,
-                "BATCH": BATCH,
-                "HEAD_DIM": HEAD_DIM,
+                "H": H,
+                "B": B,
+                "D": D,
                 "mode": mode,
                 "metric": "time",
             },
         ))
 
 @triton.testing.perf_report(configs)
-def bench_attention(BATCH, H, N_CTX, HEAD_DIM, mode, provider, device=DEVICE, metric="tflops"):
+def bench_attention(B, H, L, D, mode, provider, device=DEVICE, metric="tflops"):
     assert mode in ["fwd", "bwd"]
     dtype = torch.float16
 
@@ -450,10 +431,10 @@ def bench_attention(BATCH, H, N_CTX, HEAD_DIM, mode, provider, device=DEVICE, me
         torch.cuda.empty_cache()
 
     try:
-        q = torch.randn((BATCH, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        k = torch.randn((BATCH, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        v = torch.randn((BATCH, H, N_CTX, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-        b = torch.randn((BATCH, H, N_CTX, N_CTX), dtype=dtype, device=device, requires_grad=True)
+        q = torch.randn((B, H, L, L, D), dtype=dtype, device=device, requires_grad=True)
+        k = torch.randn((B, H, L, L, D), dtype=dtype, device=device, requires_grad=True)
+        v = torch.randn((B, H, L, L, D), dtype=dtype, device=device, requires_grad=True)
+        b = torch.randn((B, H, L, L), dtype=dtype, device=device, requires_grad=True)
         sm_scale = 1.3
 
         if "triton" in provider:
@@ -503,7 +484,7 @@ def bench_attention(BATCH, H, N_CTX, HEAD_DIM, mode, provider, device=DEVICE, me
             return ms  # Return raw milliseconds
         else:  # tflops
             raise NotImplementedError("Haven't done the FLOPs metric for Triangular Attn yet")
-            flops_per_matmul = 2.0 * BATCH * H * N_CTX * N_CTX * HEAD_DIM
+            flops_per_matmul = 2.0 * B * H * L * L * D
             total_flops = 2 * flops_per_matmul
             if mode == "bwd":
                 total_flops *= 2.5  # Additional computations for backward pass
@@ -522,13 +503,13 @@ def reference_tt_attn(q, k, v, b, sm_scale):
 
     If a debugging_dict is provided, we will check intermediate values against the debugging_dict.
     """
-    Z, H, N_CTX = q.shape[:3]
+    Z, H, L = q.shape[:3]
     # Step 1: QK^T. 
     qk = torch.matmul(q, k.transpose(-2, -1))
 
     # Step 2: Scale and add bias--attn scaling happens before bias addition
     qk_scaled = qk * sm_scale 
-    qk_scaled_bias = qk_scaled + b.view(Z, H, 1, N_CTX, N_CTX)
+    qk_scaled_bias = qk_scaled + b.view(Z, H, 1, L, L)
 
     # Step 3a: Softmax (builtin)
     p = torch.softmax(qk_scaled_bias.float(), dim=-1).to(v.dtype)
@@ -540,5 +521,5 @@ def reference_tt_attn(q, k, v, b, sm_scale):
 if __name__ == "__main__":
     # Run benchmarks (only works on post-Ampere GPUs)
     bench_attention.run(save_path=".", print_data=True)
-    test_op(Z=2, H=4, N_CTX=128, HEAD_DIM=32, dtype=torch.float16)
+    test_op(Z=2, H=4, L=128, HEAD_DIM=32, dtype=torch.float16)
     # output_dict = test_attention_intermediate_values()
